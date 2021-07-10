1db0871321d815650c0c4bd667b991ad16fca134
Akshat-Kumar-0610 <akshat8810@gmail.com>
2021-07-10 23:06:14 +0530

    bug fixes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,9 +6,11 @@ const auth = async (req,res, next)=>{
         const token = req.header('Authorization').replace('Bearer ','')
         const decoded = jwt.verify(token,'taskmanager')
         const user = await User.findOne({ _id:decoded._id,'tokens.token':token})
+        console.log(user)
         if(!user){
-            throw new Error
+            throw new Error()
         }
+        req.token = token
         req.user=user
         next()
     
--- src/routers/user.js
+++ src/routers/user.js
@@ -26,14 +26,23 @@ router.post('/users/login', async(req,res)=>{
     }
 })
 
-router.get('/users', auth ,async (req,res)=>{
+router.post('/users/logout',auth, async(req,res)=>{
     try{
-        const users = await User.find({})
-        res.send(users)
+        //console.log(req.user.tokens)
+        req.user.tokens=req.user.tokens.filter((token)=>{
+            return token.token!==req.token
+        })
+        //console.log(req.user)
+        await req.user.save()
+        res.send()
     }catch(e){
-        res.status(500).send(e)
+        console.log(e)
+        res.status(500).send()
     }
+})
 
+router.get('/users/me', auth ,async (req,res)=>{
+    res.send(req.user)
 })
 
 router.get('/users/:id',async (req,res)=>{
